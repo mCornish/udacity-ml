77b5cc00dce7138369466fce7f4ea9cef46d448d
Mike Cornish <cornishmw@gmail.com>
2018-06-23 17:12:22 -0400

    Completes SVM Mini-Project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 """ 
     This is the code to accompany the Lesson 2 (SVM) mini-project.
@@ -7,7 +7,7 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -24,7 +24,34 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+print()
 
+import math
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from collections import Counter
+
+# Decrease size of training dataset
+# features_train = features_train[: math.floor(len(features_train)/100)] 
+# labels_train = labels_train[: math.floor(len(labels_train)/100)] 
+
+clf = SVC(kernel='rbf', C=10000)
+
+t_time = time()
+clf.fit(features_train, labels_train)
+print('Training time:', round(time() - t_time, 3), 's')
+
+p_time = time()
+pred = clf.predict(features_test)
+print('Prediction time:', round(time() - p_time, 3), 's')
+
+score = accuracy_score(labels_test, pred)
+
+print('Accuracy: ', score)
+print('Prediction 10: ', pred[10])
+print('Prediction 26: ', pred[26])
+print('Prediction 50: ', pred[50])
+print('Counts (1 => Chris, 0 => Sarah): ', Counter(pred))
 #########################################################
 
 
