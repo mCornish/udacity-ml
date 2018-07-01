763f0ed0588cc4430292fe6023963a5eb9d57903
Mike Cornish <cornishmw@gmail.com>
2018-07-01 09:13:43 -0400

    Completes Outliers Mini-project

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 
 def outlierCleaner(predictions, ages, net_worths):
@@ -14,7 +14,20 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = []
 
     ### your code goes here
+    import math
+    coll = list(zip(predictions, ages, net_worths))
 
+    with_errors = []
+    for pred, age, worth in coll:
+        error = abs(worth - pred)
+        with_errors.append((age[0], worth[0], error[0]))
+
+    sorted_errors = sorted(with_errors, key=lambda tup: tup[2])
+
+    removal_count = math.floor(len(sorted_errors) * .1)
+    new_len = len(sorted_errors) - removal_count
     
+    cleaned_data = sorted_errors[0:new_len]
+
     return cleaned_data
 
