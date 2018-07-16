b3e9c733d1bc79102649df304dff4e0e18652d6e
Mike Cornish <cornishmw@gmail.com>
2018-07-16 07:47:07 -0400

    Adds Final project progress

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -23,8 +23,7 @@ from sklearn.model_selection import train_test_split
 features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
-                             stop_words='english')
+vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5, stop_words='english')
 features_train = vectorizer.fit_transform(features_train)
 features_test  = vectorizer.transform(features_test).toarray()
 
